a394c0105ad9047e7fe72a6440b7460d0487d7fa
kishanpipaliya <gamemovement192@gmail.com>
2023-02-14 13:16:27 +0530

    push

--- app/src/main/java/com/example/moneymanagement/activity/MainActivity.kt
+++ app/src/main/java/com/example/moneymanagement/activity/MainActivity.kt
@@ -12,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
+
         binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
